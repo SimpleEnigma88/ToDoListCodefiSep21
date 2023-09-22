6d12e61bb2adacad853767a57648c52b2dbd988a
Robert Andrew Moore <ra.moore573@gmail.com>
2023-09-22 12:03:41 -0500

    asdfsafasdfasdfasdf

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // create note, add to array, change to JSON store to local.   
+
+//store array to localStorage
+
+function storeTaskList(taskList) {
+    localStorage.setItem(taskList, JSON.stringify(taskList));
+}
+
+
+
+function getTaskList() {
+    return JSON.parse(localStorage.getItem('taskList'));
+}
